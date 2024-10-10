3ef1315e07889b57126ab5b0aff52a49bbb28941
SJY <bangstation@naver.com>
2024-10-10 11:13:24 +0900

    n<0부분 삭제됨

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 // computes a factorial of a given positive integer
 function factorial(n) { // 5! = 5*4*3*2*1 = 120
 
-    if( n < 0 ) {
-        throw new Error('n must be a positive integer!');
-    }
+    // if( n < 0 ) {
+    //     throw new Error('n must be a positive integer!');
+    // }
 
 
     if( n === 0 ) {
