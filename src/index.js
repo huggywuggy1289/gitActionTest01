92d567a16b9babc763d776b78b0600c18af23cec
SJY <bangstation@naver.com>
2024-10-10 10:50:56 +0900

    second commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 function factorial(n) { // 5! = 5*4*3*2*1 = 120
 
     if( n < 0 ) {
-        throw new Error('n must be a positive integer');
+        throw new Error('n must be a positive integer!');
     }
 
 
